d2e12fd17e052c11130423a0b76868d2963396ce
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-08-16 13:19:54 +0100

    The file processing channel was set up and injected into the result constructor which helped to read, copy and upload files.

--- src/TennisBookings.Web/Areas/Admin/Controllers/ResultsController.cs
+++ src/TennisBookings.Web/Areas/Admin/Controllers/ResultsController.cs
@@ -1,11 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TennisBookings.ResultsProcessing;
+using TennisBookings.Web.BackgroundServices;
 
 namespace TennisBookings.Web.Areas.Admin.Controllers
 {
@@ -16,15 +19,27 @@ namespace TennisBookings.Web.Areas.Admin.Controllers
     {
         private readonly IResultProcessor _resultProcessor;
         private readonly ILogger<ResultsController> _logger;
+        private readonly FileProcessingChannel _fileProcessingChannel;
 
-        public ResultsController(IResultProcessor resultProcessor, ILogger<ResultsController> logger)
+        public ResultsController(
+            IResultProcessor resultProcessor,
+            ILogger<ResultsController> logger,
+            //This is where we inject the FileProcessingChannel
+            FileProcessingChannel fileProcessingChannel)
         {
             _resultProcessor = resultProcessor;
             _logger = logger;
+            _fileProcessingChannel = fileProcessingChannel;
         }
 
         [HttpGet]
         public IActionResult UploadResults()
+        {
+            return View();
+        }
+
+        [HttpGet("v2")]
+        public IActionResult UploadResultsV2()
         {
             return View();
         }
@@ -38,20 +53,15 @@ namespace TennisBookings.Web.Areas.Admin.Controllers
             {
                 var fileName = Path.GetTempFileName(); // Upload to a temp file path
 
-                try
-                {
-                    await using var stream = new FileStream(fileName, FileMode.Create);
+                await using var stream = new FileStream(fileName, FileMode.Create);
 
-                    await file.CopyToAsync(stream);
+                await file.CopyToAsync(stream);
 
-                    stream.Position = 0;
+                stream.Position = 0;
 
-                    await _resultProcessor.ProcessAsync(stream);
-                }
-                finally
-                {
-                    System.IO.File.Delete(fileName); // Delete the temp file
-                }
+                await _resultProcessor.ProcessAsync(stream);
+
+                System.IO.File.Delete(fileName); // Delete the temp file
             }
 
             sw.Stop();
@@ -60,6 +70,53 @@ namespace TennisBookings.Web.Areas.Admin.Controllers
                 $"was {sw.ElapsedMilliseconds}ms.");
 
             return RedirectToAction("UploadComplete");
+        }
+
+        [HttpPost("FileUploadV2")]
+        public async Task<IActionResult> FileUploadV2(IFormFile file, CancellationToken cancellationToken)
+        {
+            //This times the response.
+            var sw = Stopwatch.StartNew();
+
+            //This block of code helps to confirm that the file contains data
+            if (file is object && file.Length > 0)
+            {
+                //This creates a temporary file name for the file
+                var fileName = Path.GetTempFileName();
+
+                //This copies creates, writes and copies data from the temporary file.
+                using (var stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    await file.CopyToAsync(stream, cancellationToken);
+                }
+
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                //This ensures that if the program can't handle the request after 3 seconds it is terminated
+                cts.CancelAfter(TimeSpan.FromSeconds(3)); // wait max 3 seconds
+
+                try
+                {
+                    //This is where we try to write the file name into the FileProcessingChannel
+                    var fileWritten = await _fileProcessingChannel.AddFileAsync(fileName, cts.Token);
+
+                    if (fileWritten)
+                    {
+                        //after the file is written we stop the timer and we measure the time taken.
+                        sw.Stop();
+
+                        _logger.LogInformation($"Time for result upload was {sw.ElapsedMilliseconds}ms.");
+
+                        return RedirectToAction("UploadComplete");
+                    }
+                }
+                //If the file doesn't send, we delete it and classify it as an upload failure.
+                catch (OperationCanceledException) when (cts.IsCancellationRequested)
+                {
+                    System.IO.File.Delete(fileName); // Delete the temp file to cleanup
+                }
+            }
+
+            return RedirectToAction("UploadFailed");
         }
 
         [HttpGet("FileUploadComplete")]
---
+++ src/TennisBookings.Web/BackgroundServices/FileProcessingChannel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TennisBookings.Web.BackgroundServices
+{
+    public class FileProcessingChannel
+    {
+        //The line of code below specifies the maximum number of messages that can be transmitted through the channel at any point or the other.
+        private const int MaxMessagesInChannel = 100;
+
+        private readonly Channel<string> _channel;
+        private readonly ILogger<FileProcessingChannel> _logger;
+
+        public FileProcessingChannel(ILogger<FileProcessingChannel> logger)
+        {
+            //This is where we specifiy that we can have multiple people uploading files
+            //The BoundedChannelOption is what ensures that users wait if there the maximum number of requests on the channel has been read.
+            var options = new BoundedChannelOptions(MaxMessagesInChannel)
+            {
+                SingleWriter = false,
+                SingleReader = true                
+            };
+
+            //This is where we acccept the temporary file name of the uploaded file (as a string)
+            _channel = Channel.CreateBounded<string>(options);
+
+            _logger = logger;
+        }
+
+        //This handles the process of adding a file, the filename is accepted as a parameter along with a cancellation token
+        public async Task<bool> AddFileAsync(string fileName, CancellationToken ct = default)
+        {
+            //This code block ensures that the file will be written to the channel as long as the maximum limit hasn't been reached or it will wait for space to be made available
+            while (await _channel.Writer.WaitToWriteAsync(ct) && !ct.IsCancellationRequested)
+            {
+                if (_channel.Writer.TryWrite(fileName))
+                {
+                    Log.ChannelMessageWritten(_logger, fileName);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        //This method below simply calls and returns the IAsyncEnumerable from the read or Async method that is exposed to the file processing channel reader.
+        //Note that the file processing channel is registered as a singleton service within the dependency injection container for this web app.
+        public IAsyncEnumerable<string> ReadAllAsync(CancellationToken ct = default) =>
+            _channel.Reader.ReadAllAsync(ct);
+
+        public bool TryCompleteWriter(Exception ex = null) => _channel.Writer.TryComplete(ex);
+
+        internal static class EventIds
+        {
+            public static readonly EventId ChannelMessageWritten = new EventId(100, "ChannelMessageWritten");
+        }
+
+        private static class Log
+        {
+            private static readonly Action<ILogger, string, Exception> _channelMessageWritten = LoggerMessage.Define<string>(
+                LogLevel.Information,
+                EventIds.ChannelMessageWritten,
+                "Filename {FileName} was written to the channel.");
+
+            public static void ChannelMessageWritten(ILogger logger, string fileName)
+            {
+                _channelMessageWritten(logger, fileName, null);
+            }
+        }
+    }
+}
